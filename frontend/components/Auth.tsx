9d79161e92810409e9704951541e096d524fd431
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-05-28 19:37:06 -0700

    feature: supabase email auth

--- frontend/components/Auth.tsx
+++ frontend/components/Auth.tsx
@@ -3,7 +3,7 @@ import { Alert, StyleSheet, View } from "react-native";
 import { supabase } from "../lib/supabase";
 import { Button, Input } from "@rneui/themed";
 
-export default function Auth() {
+export default function EmailForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,32 +1,76 @@
-import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient, processLock } from "@supabase/supabase-js";
+import * as SecureStore from "expo-secure-store";
+import * as aesjs from "aes-js";
+import "react-native-get-random-values";
 
-const supabaseUrl =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6a3Nob2lubGFxa3JkdXZsbm54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4ODg5MjEsImV4cCI6MjA2MzQ2NDkyMX0.r2CdxBfAMFefVq6-NCGZMk5bKJpcMRZMG-RbSYfDh-4";
+// As Expo's SecureStore does not support values larger than 2048
+// bytes, an AES-256 key is generated and stored in SecureStore, while
+// it is used to encrypt/decrypt values stored in AsyncStorage.
+class LargeSecureStore {
+  private async _encrypt(key: string, value: string) {
+    const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8));
+
+    const cipher = new aesjs.ModeOfOperation.ctr(
+      encryptionKey,
+      new aesjs.Counter(1),
+    );
+    const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
+
+    await SecureStore.setItemAsync(
+      key,
+      aesjs.utils.hex.fromBytes(encryptionKey),
+    );
+
+    return aesjs.utils.hex.fromBytes(encryptedBytes);
+  }
+
+  private async _decrypt(key: string, value: string) {
+    const encryptionKeyHex = await SecureStore.getItemAsync(key);
+    if (!encryptionKeyHex) {
+      return encryptionKeyHex;
+    }
+
+    const cipher = new aesjs.ModeOfOperation.ctr(
+      aesjs.utils.hex.toBytes(encryptionKeyHex),
+      new aesjs.Counter(1),
+    );
+    const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
+
+    return aesjs.utils.utf8.fromBytes(decryptedBytes);
+  }
+
+  async getItem(key: string) {
+    const encrypted = await AsyncStorage.getItem(key);
+    if (!encrypted) {
+      return encrypted;
+    }
+
+    return await this._decrypt(key, encrypted);
+  }
+
+  async removeItem(key: string) {
+    await AsyncStorage.removeItem(key);
+    await SecureStore.deleteItemAsync(key);
+  }
+
+  async setItem(key: string, value: string) {
+    const encrypted = await this._encrypt(key, value);
+
+    await AsyncStorage.setItem(key, encrypted);
+  }
+}
+
+const supabaseUrl = "https://pzkshoinlaqkrduvlnnx.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6a3Nob2lubGFxa3JkdXZsbm54Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Nzg4ODkyMSwiZXhwIjoyMDYzNDY0OTIxfQ.20u_Sb4OiAMPCWMJsdxKOIwKrC-DeOJJnXBaiwlE1mw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: new LargeSecureStore(),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
-    lock: processLock,
   },
-});
-
-// Tells Supabase Auth to continuously refresh the session automatically
-// if the app is in the foreground. When this is added, you will continue
-// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
-// `SIGNED_OUT` event if the user's session is terminated. This should
-// only be registered once.
-AppState.addEventListener("change", (state) => {
-  if (state === "active") {
-    supabase.auth.startAutoRefresh();
-  } else {
-    supabase.auth.stopAutoRefresh();
-  }
 });
